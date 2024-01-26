3ffa89f122804f14c070a76add79804537d55be6
mmaksi <mmaksi.dev@gmail.com>
2024-01-26 11:35:42 +0100

    added solution for creating a custom matcher to test input fields validity

--- src/utils/customMatchers.tsx
+++ src/utils/customMatchers.tsx
@@ -1,5 +1,34 @@
 import { Item } from "@/types/Item";
 
+/**
+ * TODO 1: Create a custom matcher to test strings to have letters only
+ * TODO 2: Test that matcher on a different strings
+ * TODO 2: Test that matcher on the input fields in the UserForm.test.js file
+ * Hint: you have to use the fireEvent function from @testing-library/react
+ */
+export const toContainOnlyLetters = (received: string) => {
+  const validate = (str: string) => {
+    const letterRegex = /^[a-zA-Z\s]+$/;
+    return letterRegex.test(str);
+  };
+
+  const pass = validate(received);
+
+  switch (pass) {
+    case true:
+      return {
+        message: () => `expected "${received}" to contain only letters.`,
+        pass: true,
+      };
+    case false:
+      return {
+        message: () =>
+          `expected "${received}" not to contain symbols or numbers.`,
+        pass: false,
+      };
+  }
+};
+
 export const toHaveTotalPrice = (
   received: Item[],
   expectedTotalPrice: number
@@ -26,4 +55,4 @@ export const toHaveTotalPrice = (
   }
 };
 
-expect.extend({ toHaveTotalPrice });
+expect.extend({ toHaveTotalPrice, toContainOnlyLetters });
